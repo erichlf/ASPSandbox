8cf722f596ebf525111a414ca4408ba8a913aee5
Erich L Foster <erichlf@gmail.com>
2013-08-30 11:21:36 +0200

    added a steady stokes solver.

--- src/solvers/__init__.py
+++ src/solvers/__init__.py
@@ -2,7 +2,7 @@ __author__ = "Erich L Foster <efoster@bcamath.org>"
 __date__ = "2013-08-27"
 
 # List of solvers
-solvers = ['NSE', 'Stokes', 'LinearSWE', 'SWE']
+solvers = ['NSE', 'SteadyStokes', 'Stokes', 'LinearSWE', 'SWE']
 
 # Wrapper for solver classes
 def Solver(name, options):
