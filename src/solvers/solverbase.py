df7a2c1840b791b1a271321457848bff04f38d5b
erichlf <erichlf@gmail.com>
2014-05-20 13:51:07 +0200

    Added streamline diffusion for stabilization.

--- src/solvers/solverbase.py
+++ src/solvers/solverbase.py
@@ -135,9 +135,15 @@ class SolverBase:
           d1, d2 = self.stabilization_parameters(U_,eta_,h)
 
           #add stabilization
-          R1, R2 = self.strong_residual(U_alpha,U_alpha,eta_alpha)
-          Rv1, Rv2 = self.strong_residual(U_alpha,v,chi)
-          F += d1*inner(R1 - F1, Rv1)*dx + d2*(R2 - F2)*Rv2*dx
+          if 'object' in dir(self):
+              #R1, R2, z1, z2 = self.strong_residual(U_alpha,U_alpha,eta_alpha)
+              #Rv1, Rv2, zv1, zv2 = self.strong_residual(U_alpha,v,chi)
+              #F += d1*inner(R1 + z1 - F1, Rv1)*dx + d2*(R2 + z2 - F2)*Rv2*dx
+              F += h**(3./2.)*(inner(grad(U_alpha),grad(v)) + inner(grad(eta),grad(chi)))*dx
+          else:
+              R1, R2 = self.strong_residual(U_alpha,U_alpha,eta_alpha)
+              Rv1, Rv2 = self.strong_residual(U_alpha,v,chi)
+              F += d1*inner(R1 - F1, Rv1)*dx + d2*(R2 - F2)*Rv2*dx
 
         U_, eta_ = self.timeStepper(problem, t, T, self.dt, W, w, w_, U_, eta_, F)
         return U_, eta_
