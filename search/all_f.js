d69a208c32c05b1329825cb8a51fb15eb232e3d6
erichlf <erichlf@gmail.com>
2014-08-06 15:25:31 +0200

    Updated some Heat equation related problems and the solver.

--- search/all_f.js
+++ search/all_f.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['x_5fslipboundary',['X_SlipBoundary',['../classproblems_1_1Pool_1_1X__SlipBoundary.htm',1,'problems::Pool']]],
-  ['x_5fslipboundary',['X_SlipBoundary',['../classproblems_1_1PeregrineSoliton_1_1X__SlipBoundary.htm',1,'problems::PeregrineSoliton']]]
+  ['x_5fslipboundary',['X_SlipBoundary',['../classproblems_1_1PeregrineSoliton_1_1X__SlipBoundary.htm',1,'problems::PeregrineSoliton']]],
+  ['x_5fslipboundary',['X_SlipBoundary',['../classproblems_1_1Pool_1_1X__SlipBoundary.htm',1,'problems::Pool']]]
 ];
--- search/classes_7.js
+++ search/classes_7.js
@@ -1,13 +1,14 @@
 var searchData=
 [
-  ['solver',['Solver',['../classsolvers_1_1QGE_1_1Solver.htm',1,'solvers::QGE']]],
-  ['solver',['Solver',['../classsolvers_1_1Peregrine_1_1Solver.htm',1,'solvers::Peregrine']]],
   ['solver',['Solver',['../classsolvers_1_1SWE_1_1Solver.htm',1,'solvers::SWE']]],
-  ['solver',['Solver',['../classsolvers_1_1NSE_1_1Solver.htm',1,'solvers::NSE']]],
-  ['solver',['Solver',['../classsolvers_1_1ADR_1_1Solver.htm',1,'solvers::ADR']]],
+  ['solver',['Solver',['../classsolvers_1_1Peregrine_1_1Solver.htm',1,'solvers::Peregrine']]],
   ['solver',['Solver',['../classsolvers_1_1DensityNSE_1_1Solver.htm',1,'solvers::DensityNSE']]],
   ['solver',['Solver',['../classsolvers_1_1Heat_1_1Solver.htm',1,'solvers::Heat']]],
+  ['solver',['Solver',['../classsolvers_1_1ADR_1_1Solver.htm',1,'solvers::ADR']]],
+  ['solver',['Solver',['../classsolvers_1_1QGE_1_1Solver.htm',1,'solvers::QGE']]],
+  ['solver',['Solver',['../classsolvers_1_1NSE_1_1Solver.htm',1,'solvers::NSE']]],
   ['solverbase',['SolverBase',['../classsolvers_1_1solverbase_1_1SolverBase.htm',1,'solvers::solverbase']]],
   ['subdomain',['SubDomain',['../classSubDomain.htm',1,'']]],
-  ['surface',['Surface',['../classproblems_1_1Ground_1_1Surface.htm',1,'problems::Ground']]]
+  ['surface2d',['Surface2D',['../classproblems_1_1Ground_1_1Surface2D.htm',1,'problems::Ground']]],
+  ['surface3d',['Surface3D',['../classproblems_1_1Ground_1_1Surface3D.htm',1,'problems::Ground']]]
 ];
