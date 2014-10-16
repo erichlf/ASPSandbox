491c7eca5c3e177cba0d1bc749b747a6c8b3a9b2
erichlf <erichlf@gmail.com>
2014-10-16 16:31:49 +0200

    Adjusted Cube to the new AFES framework.

--- src/problems/Cube.py
+++ src/problems/Cube.py
@@ -1,18 +1,16 @@
 __author__ = "Kristian Valen-Sendstad <kvs@simula.no>"
 __date__ = "2009-10-01"
 __copyright__ = "Copyright (C) 2009-2010 " + __author__
-__license__  = "GNU GPL version 3 or any later version"
+__license__ = "GNU GPL version 3 or any later version"
 
 # Modified by Anders Logg, 2010.
 # Modified by Erich L Foster, 2013
 
-from problembase import *
 from Cylinder import Problem as Cylinder
-from numpy import array
 
-# this is basically the cylinder problem but the shape is a square/box
-# Problem definition
+
 class Problem(Cylinder):
+
     '''
         Purely a container to call Cylinder with the keyword argument cube. This
         will tell Cylinder.py to use a cube instead of a cylinder.
